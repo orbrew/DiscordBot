a87b2d4d32f28dca215ddf5e50972a53607ea748
orbrew <andreworbine@gmail.com>
2022-02-09 23:41:53 -0500

    added info embed data

--- cogs/info.py
+++ cogs/info.py
@@ -11,12 +11,23 @@ class Info(Cog):
 
     @command(name="userinfo", aliases=["info", "getinfo"])
     async def user_info(self, ctx, target: discord.Member = None):
-        target = target 
+        target = target
 
         embed = Embed(title="User info",
                       color=target.color,
                       timestamp=datetime.utcnow())
         embed.set_thumbnail(url=target.avatar_url)
+        
+
+        fields = [("Name", str(target), True),
+                  ("Top role", target.top_role.mention, True),
+                  ("Status", str(target.status).title(), True),
+                  ("Activity", target.activity, True),
+                  ("Account created", target.created_at.strftime("%m/%d/%Y %H:%M%S"), True),
+                  ("Joined server", target.joined_at.strftime("%m/%d/%Y %H:%M:%S"), True)]
+
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
 
         await ctx.send(embed=embed)
 
