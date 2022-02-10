e0759bbad4797490bd87dedecaa158cbf00844da
orbrew <andreworbine@gmail.com>
2022-02-09 23:58:00 -0500

    added server info embed

--- cogs/info.py
+++ cogs/info.py
@@ -17,14 +17,33 @@ class Info(Cog):
                       color=target.color,
                       timestamp=datetime.utcnow())
         embed.set_thumbnail(url=target.avatar_url)
-        
 
         fields = [("Name", str(target), True),
                   ("Top role", target.top_role.mention, True),
                   ("Status", str(target.status).title(), True),
                   ("Activity", target.activity, True),
-                  ("Account created", target.created_at.strftime("%m/%d/%Y %H:%M%S"), True),
+                  ("Account created", target.created_at.strftime(
+                      "%m/%d/%Y %H:%M%S"), True),
                   ("Joined server", target.joined_at.strftime("%m/%d/%Y %H:%M:%S"), True)]
+
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+
+        await ctx.send(embed=embed)
+
+    @command(name="serverinfo", aliases=["guild", "server", "guildinfo", "gi", "si"])
+    async def server_info(self, ctx):
+        embed = Embed(title="Server info",
+                      timestamp=datetime.utcnow())
+
+        fields = [("ID", ctx.guild.id, True),
+                  ("Owner", ctx.guild.id, True),
+                  ("Region", ctx.guild.region, True),
+                  ("Created", ctx.guild.created_at.strftime("%m/%d/%Y %H:%M:%S"), True),
+                  ("Members", len(ctx.guild.members), True),
+                  ("Text channels", len(ctx.guild.text_channels), True),
+                  ("Voice channels", len(ctx.guild.voice_channels), True),
+                  ("Roles", ctx.guild.roles, True)]
 
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
