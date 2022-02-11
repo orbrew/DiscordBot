8afa153dddca818b2a9ddb81128f7646b98bddee
orbrew <andreworbine@gmail.com>
2022-02-10 19:58:26 -0500

    info embed fixes

--- cogs/info.py
+++ cogs/info.py
@@ -9,9 +9,9 @@ class Info(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @command(name="userinfo", aliases=["info", "getinfo"])
+    @command(name="userinfo", aliases=["info", "getinfo", "ui", "user"])
     async def user_info(self, ctx, target: discord.Member = None):
-        target = target
+        target = target or ctx.author
 
         embed = Embed(title="User info",
                       color=target.color,
@@ -22,9 +22,11 @@ class Info(Cog):
                   ("Top role", target.top_role.mention, True),
                   ("Status", str(target.status).title(), True),
                   ("Activity", target.activity, True),
+                  ("ID", target.id, False),
+                  ("Joined server", target.joined_at.strftime(
+                      "%m/%d/%Y %H:%M:%S"), True),
                   ("Account created", target.created_at.strftime(
-                      "%m/%d/%Y %H:%M%S"), True),
-                  ("Joined server", target.joined_at.strftime("%m/%d/%Y %H:%M:%S"), True)]
+                      "%m/%d/%Y %H:%M:%S"), True)]
 
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
@@ -37,9 +39,10 @@ class Info(Cog):
                       timestamp=datetime.utcnow())
 
         fields = [("ID", ctx.guild.id, True),
-                  ("Owner", ctx.guild.id, True),
+                  ("Owner", ctx.guild.owner, True),
                   ("Region", ctx.guild.region, True),
-                  ("Created", ctx.guild.created_at.strftime("%m/%d/%Y %H:%M:%S"), True),
+                  ("Created", ctx.guild.created_at.strftime(
+                      "%m/%d/%Y %H:%M:%S"), True),
                   ("Members", len(ctx.guild.members), True),
                   ("Text channels", len(ctx.guild.text_channels), True),
                   ("Voice channels", len(ctx.guild.voice_channels), True),
